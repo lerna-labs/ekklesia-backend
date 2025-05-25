d83ef26e5aeb8df5a9678676b81a9055bf3b62a5
Mad Orkestra <mad@madorkestra.com>
2025-05-25 17:20:05 +0200

    Config Update

--- preprod.config.cjs
+++ preprod.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       name: "preprod-server",
       script: path.resolve(__dirname, "server.js"),
       watch: true,
-      ignore_watch: ["node_modules", "logs", "backup", "scripts", "public"],
+      ignore_watch: ["node_modules", "logs", "backup", "scripts", "public", "preprod-logs", "mainnet-logs"],
       watch_options: {
         followSymlinks: false,
         usePolling: true,
